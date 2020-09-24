7195f41465978a493a86f8ac847b1df5d7b196e0
DemieOh <demie.oh@gmail.com>
2020-09-24 14:44:12 +0900

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
 import os.path
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as dataloader
 import torch.utils.tensorboard as tensorboard
+import torchvision.models as models
 import torchvision.transforms as transforms
 
 import datasets.rtgene as rtgene
-import models.rtgene.model as models
 import modules.modules as mm
-import utils
+from utils import utils
 
 trainlist = ['s001', 's002', 's003', 's004', 's005', 's006', 's007', 's008', 's009', 's010', 's011', 's012', 's013']
 validlist = ['s014', 's015', 's016']
@@ -24,28 +25,29 @@ def main(args):
         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ])
 
-    data_type = ['left', 'right']
+    data_type = ['face']
 
     trainset = rtgene.RTGENE(root=args.root, transform=transform, subjects=trainlist, data_type=data_type)
     validset = rtgene.RTGENE(root=args.root, transform=transform, subjects=validlist, data_type=data_type)
     trainloader = dataloader.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
     validloader = dataloader.DataLoader(validset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
 
-    model = models.GazeEstimator(arch='resnet34', pretrained=True, device=args.device)
+    model = models.resnet50(pretrained=True)
+    model.fc = nn.Linear(in_features=model.fc.in_features, out_features=2)
     model = nn.DataParallel(model).to(args.device)
 
     criterion = nn.MSELoss()
     evaluator = mm.AngleAccuracy()
-    optimizer = optim.Adam(model.parameters(), lr=0.005, betas=(0.9, 0.95))
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    optimizer = optim.Adam(model.parameters(), lr=0.01, betas=(0.9, 0.95))
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, cooldown=10)
 
-    writer = tensorboard.SummaryWriter(os.path.join(args.logs, 'rt-gene-resnet34'))
+    writer = tensorboard.SummaryWriter(os.path.join(args.logs, 'head-pose-resnet50-lr0.01'))
 
     for epoch in range(args.epochs):
         args.epoch = epoch
         train(trainloader, model, criterion, evaluator, optimizer, writer, args)
-        validate(validloader, model, criterion, evaluator, writer, args)
-        scheduler.step()
+        score = validate(validloader, model, criterion, evaluator, writer, args)
+        scheduler.step(score)
         writer.add_scalar('learning rate', optimizer.param_groups[0]['lr'], epoch)
 
     writer.close()
@@ -55,13 +57,12 @@ def train(dataloader, model, criterion, evaluator, optimizer, writer, args):
 
     model.train()
     for i, batch in enumerate(dataloader):
-        left, right, headpose, targets = batch
-        left, right, headpose = left.to(args.device), right.to(args.device), headpose.to(args.device)
-        targets = targets.to(args.device)
+        face, headpose, _ = batch
+        face, headpose = face.to(args.device), headpose.to(args.device)
 
-        outputs = model(left, right, headpose)
-        loss = criterion(outputs, targets)
-        accuracy = evaluator(outputs, targets)
+        outputs = model(face)
+        loss = criterion(outputs, headpose)
+        accuracy = evaluator(outputs, headpose)
 
         optimizer.zero_grad()
         loss.backward()
@@ -76,22 +77,26 @@ def train(dataloader, model, criterion, evaluator, optimizer, writer, args):
 
 def validate(dataloader, model, criterion, evaluator, writer, args):
 
+    res = []
     model.eval()
     with torch.no_grad():
         for i, batch in enumerate(dataloader):
-            left, right, headpose, targets = batch
-            left, right, headpose = left.to(args.device), right.to(args.device), headpose.to(args.device)
-            targets = targets.to(args.device)
+            face, headpose, _ = batch
+            face, headpose = face.to(args.device), headpose.to(args.device)
 
-            outputs = model(left, right, headpose)
-            loss = criterion(outputs, targets)
-            accuracy = evaluator(outputs, targets)
+            outputs = model(face)
+            loss = criterion(outputs, headpose)
+            accuracy = evaluator(outputs, headpose)
+
+            res.append(loss.item())
 
             writer.add_scalar('validation loss', loss.item(), args.epoch * len(dataloader) + i)
             writer.add_scalar('validation accuracy', accuracy.item(), args.epoch * len(dataloader) + i)
 
             print(f'Epoch[{args.epoch + 1:4d}/{args.epochs:4d}] - batch[{i + 1:4d}/{len(dataloader):4d}]'
                   f' - loss: {loss.item():7.3f} - accuracy: {accuracy.item():7.3f}')
+
+    return np.mean(res)
 
 
 if __name__ == '__main__':
---
+++ models/modules.py
@@ -0,0 +1,41 @@
+from PIL import Image
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+
+class SpatialAttention(nn.Module):
+
+    def __init__(self, in_channels):
+        super(SpatialAttention, self).__init__()
+        self.conv1x1 = nn.Conv2d(in_channels=in_channels, out_channels=1, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        x = self.conv1x1(x)
+        return x
+
+
+def view(image):
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+
+
+image = Image.open('example.jpg').convert('RGB')
+view(image)
+
+transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+])
+
+image = transform(image)
+
+down_channels = SpatialAttention(3)
+image = down_channels(image.unsqueeze(0)).squeeze(0)
+
+view(image.detach().numpy().transpose(1, 2, 0))
+
+breakpoint()
