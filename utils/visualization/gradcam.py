73b509104d9d536d6ef921447359cc1fb78ce241
DemieOh <demie.oh@gmail.com>
2020-09-25 17:51:45 +0900

    Initial commit

--- utils/visualization/gradcam.py
+++ utils/visualization/gradcam.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 
@@ -50,10 +52,10 @@ class ModelFeatureExtractor:
 
 
 class GradCam:
+
     def __init__(self, model, feature_module, target_layer_names):
         self.model = model
         self.model.eval()
-
         self.feature_module = feature_module
         self.model_extractor = ModelFeatureExtractor(self.model, self.feature_module, target_layer_names)
 
@@ -61,40 +63,34 @@ class GradCam:
         return self.model(x)
 
     def __call__(self, image, index=None):
-
         features, output = self.model_extractor(image)
+        index = torch.argmax(output).item() if index is None else index
 
-        if index is None:
-            index = np.argmax(output.cpu().data.numpy())
-
-        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
+        one_hot = torch.zeros((1, output.size()[-1]))
         one_hot[0][index] = 1
-        one_hot = torch.from_numpy(one_hot).requires_grad_(True)
+        one_hot.requires_grad = True
         one_hot = torch.sum(one_hot * output)
 
         self.feature_module.zero_grad()
         self.model.zero_grad()
         one_hot.backward(retain_graph=True)
 
-        grads_val = self.model_extractor.get_gradients()[-1].cpu().data.numpy()
+        target = features[-1].squeeze()
+        weights = nn.AdaptiveAvgPool2d(1)(self.model_extractor.get_gradients()[-1]).squeeze()
 
-        target = features[-1]
-        target = target.cpu().data.numpy()[0, :]
-
-        weights = np.mean(grads_val, axis=(2, 3))[0, :]
-        mask = np.zeros(target.shape[1:], dtype=np.float32)
-
+        mask = torch.zeros(target.shape[1:])
         for i, w in enumerate(weights):
             mask += w * target[i, :, :]
 
-        mask = np.maximum(mask, 0)
-        mask = Image.fromarray(mask).resize(image.shape[2:])
-        mask = mask - np.min(mask)
-        mask = mask / np.max(mask)
+        mask = mask.clamp(0, 1).view(1, 1, mask.shape[0], mask.shape[1])
+        mask = F.interpolate(mask, size=image.shape[2:], mode='bicubic', align_corners=False).squeeze()
+        mask = mask - torch.min(mask)
+        mask = mask / torch.max(mask)
+
         return mask
 
 
-def view_grad_cam(image, mask, filename='GradCAM.png'):
+def view_activation_map(image, mask, filename='visualized_activation_map.png'):
     image = np.array(image, dtype=np.float32) / 255
     heatmap = plt.get_cmap('jet')(mask)[:, :, :3].astype(np.float32)
 
@@ -114,20 +110,26 @@ if __name__ == '__main__':
     import torchvision.models as models
     import torchvision.transforms as transforms
 
+    device = 'cuda:0'
+
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ])
 
-    model = models.resnet50(pretrained=True)
-    grad_cam = GradCam(model=model, feature_module=model.layer4, target_layer_names=["2"])
+    model = models.resnet18(pretrained=True)
+    model.fc = nn.Linear(in_features=model.fc.in_features, out_features=2)
+    model.to(device)
+    visualizer = GradCam(model=model, feature_module=model.layer4, target_layer_names=["1"])
 
     image = Image.open('example.jpg').convert('RGB').resize((224, 224))
-    transformed_image = transform(image).unsqueeze(0).requires_grad_(True)
+    transformed_image = transform(image).unsqueeze(0)
+    transformed_image.requires_grad = True
+    transformed_image.to(device)
 
     # if target_index = None, returns the map for the highest scoring category.
     # otherwise, targets requires index.
     target_index = None
-    mask = grad_cam(transformed_image, target_index)
+    mask = visualizer(transformed_image, target_index)
 
-    view_grad_cam(image, mask)
+    view_activation_map(image, mask.detach().numpy())
