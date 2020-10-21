3b7d8751138d8fd71586fb828d07e87d6f99a358
DemieOh <demie.oh@gmail.com>
2020-10-22 01:06:34 +0900

    Initial commit

--- utils/callbacks/callbacks.py
+++ utils/callbacks/callbacks.py
@@ -1,11 +1,32 @@
-from . import functional as F
+# from . import functional as F
 
 
-class Checkpointer:
+class CheckPoint:
 
-    def __init__(self, path, filename):
-        self.path = path
-        self.filename = filename
+    def __init__(self, filepath, monitor, save_best_only=False, save_weights_only=False, mode='auto', verbose=0):
+        self.filepath = filepath
+        self.monitor = monitor
+        self.save_best_only = save_best_only
+        self.save_weights_only = save_weights_only
+        self.mode = mode
+        self.verbose = verbose
 
-    def __call__(self, state, score):
-        F.save_checkpoint(state, self.path, self.filename, True)
+
+class EarlyStop:
+
+    def __init__(self, monitor, delta=0, patience=0, baseline=None, mode='auto', verbose=0):
+        self.monitor = monitor
+        self.delta = delta
+        self.patience = patience
+        self.baseline = baseline
+        self.mode = mode
+        self.verbose = verbose
+
+
+class TensorBoard:
+
+    def __init__(self, writer):
+        self.writer = writer
+
+    def __call__(self, loss, score):
+        pass
