9c99bb65777f9bd9d24cad3725b6e7bac428e737
Demian Oh <demie.oh@gmail.com>
2020-11-25 18:12:28 +0900

    Initial commit

--- main.py
+++ main.py
@@ -1,65 +1,44 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as loader
 
-import datasets
-import models
-import modules
-import utils
+epochs = 1000
 
 
-def main(args):
+def main():
 
-    trainset = args.initialize_object('dataset', datasets, train=True, transform=utils.transform)
-    validset = args.initialize_object('dataset', datasets, train=False, transform=utils.transform)
-    trainloader = args.initialize_object('loader', loader, trainset, shuffle=True)
-    validloader = args.initialize_object('loader', loader, validset, shuffle=False)
+    trainset = ...
+    validset = ...
+    test_set = ...
+    trainloader = ...
+    validloader = ...
+    test_loader = ...
 
-    model = args.initialize_object('model', models)
-    model.to(args.device)
+    model = ...
 
-    criterion = args.initialize_object('criterion', nn)
-    evaluator = args.initialize_object('evaluator', modules)
-    optimizer = args.initialize_object('optimizer', optim, model.parameters())
-    scheduler = args.initialize_object('scheduler', optim.lr_scheduler, optimizer)
+    criterion = ...
+    evaluator = ...
+    optimizer = ...
+    scheduler = ...
 
-    for epoch in range(args.epochs):
-        args.epoch = epoch
-        train(trainloader, model, criterion, evaluator, optimizer, args)
-        score = validate(validloader, model, criterion, evaluator, args)
-        scheduler.step(score)
+    callbacks = ...
 
-
-def train(dataloader, model, criterion, evaluator, optimizer, args):
-    model.train()
-    for i, (data, targets) in enumerate(dataloader):
-        data, targets = data.to(args.device), targets.to(args.device)
-
-        outputs = model(data)
-        loss = criterion(outputs, targets)
-        score = evaluator(outputs, targets)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    for epoch in range(epochs):
+        train()
+        validate()
+        if callbacks:
+            break
+        test()
 
 
-def validate(dataloader, model, criterion, evaluator, args):
-    scores = []
-    model.eval()
-    with torch.no_grad():
-        for i, (data, targets) in enumerate(dataloader):
-            data, targets = data.to(args.device), targets.to(args.device)
+def train():
+    pass
 
-            outputs = model(data)
-            loss = criterion(outputs, targets)
-            score = evaluator(outputs, targets)
-            scores.append(score.item())
 
-    return np.nanmean(scores)
+def validate():
+    pass
+
+
+def test():
+    pass
 
 
 if __name__ == '__main__':
-    pass
+    main()
