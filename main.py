29ae151c690766ad4af8096e94fb3460c36b8763
Demian Oh <demie.oh@gmail.com>
2020-11-25 20:31:42 +0900

    Initial commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import datasets
 import models
 import modules
 
+root = '/mnt/datasets/RT-GENE'
 epochs = 1000
 
 
@@ -19,12 +20,12 @@ def main():
         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ])
 
-    trainset = datasets.RTGENE(..., transform=transform, subjects=[], data_type=[])
-    validset = datasets.RTGENE(..., transform=transform, subjects=[], data_type=[])
-    test_set = datasets.RTGENE(..., transform=transform, subjects=[], data_type=[])
-    trainloader = loader.DataLoader(trainset, ...)
-    validloader = loader.DataLoader(validset, ...)
-    test_loader = loader.DataLoader(test_set, ...)
+    trainset = datasets.RTGENE(root, transform=transform, subjects=[], data_type=[])
+    validset = datasets.RTGENE(root, transform=transform, subjects=[], data_type=[])
+    test_set = datasets.RTGENE(root, transform=transform, subjects=[], data_type=[])
+    trainloader = loader.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=8)
+    validloader = loader.DataLoader(validset, batch_size=64, shuffle=False, num_workers=8)
+    test_loader = loader.DataLoader(test_set, batch_size=1, shuffle=False, num_workers=8)
 
     model = models.EfficientDet()
 
@@ -43,11 +44,13 @@ def main():
         if callbacks.early_stop:
             break
 
-    test(test_loader, model)
+    test(test_loader, model, criterion)
 
 
 def train(dataloader, model, optimizer, criterion, evaluator, callbacks):
+
     model.train()
+
     for idx, data in enumerate(dataloader):
         inputs, targets = data
         inputs, targets = inputs.to('cuda:0'), targets.to('cuda:0')
@@ -60,12 +63,14 @@ def train(dataloader, model, optimizer, criterion, evaluator, callbacks):
         loss.backward()
         optimizer.step()
 
-        callbacks.history()
+        callbacks.history(loss.item(), score.item())
         callbacks.checkpoint()
 
 
 def validate(dataloader, model, criterion, evaluator, callbacks):
+
     model.eval()
+
     for idx, data in enumerate(dataloader):
         inputs, targets = data
         inputs, targets = inputs.to('cuda:0'), targets.to('cuda:0')
@@ -74,16 +79,29 @@ def validate(dataloader, model, criterion, evaluator, callbacks):
         loss = criterion(outputs, targets)
         score = evaluator(outputs, targets)
 
-        callbacks.history()
+        callbacks.history(loss.item(), score.item())
 
 
-def test(dataloader, model):
+def test(dataloader, model, criterion):
+
+    predictions = []
+    grounds = []
+
     model.eval()
+
     for idx, data in enumerate(dataloader):
         inputs, targets = data
         inputs, targets = inputs.to('cuda:0'), targets.to('cuda:0')
 
         outputs = model(inputs)
+        predictions.append(outputs)
+        grounds.append(targets)
+
+    predictions = torch.stack(predictions, dim=0)
+    grounds = torch.stack(grounds, dim=0)
+    score = criterion(predictions, grounds)
+
+    return score
 
 
 if __name__ == '__main__':
