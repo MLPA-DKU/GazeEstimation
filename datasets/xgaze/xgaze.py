53814fc81928e9a5772023abb978845a30833db5
winterchild <demie.oh@gmail.com>
2021-04-30 18:05:56 +0900

    Initial commit

--- datasets/xgaze/xgaze.py
+++ datasets/xgaze/xgaze.py
@@ -1,137 +1,49 @@
-import numpy as np
-import h5py
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
 import os
 import json
-import random
-from typing import List
+import h5py
 
-trans_train = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
-    ])
-
-trans = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
-    ])
+from torchvision.datasets import VisionDataset
 
 
-def get_train_loader(data_dir, batch_size, num_workers=4, is_shuffle=True):
-    # load dataset
-    refer_list_file = os.path.join(data_dir, 'train_test_split.json')
-    print('load the train file list from: ', refer_list_file)
+class XGaze(VisionDataset):
 
-    with open(refer_list_file, 'r') as f:
-        datastore = json.load(f)
+    def __init__(self, root, train=True, transform=None, target_transform=None):
+        super(XGaze, self).__init__(root, transform=transform, target_transform=target_transform)
 
-    # there are three subsets for ETH-XGaze dataset: train, test and test_person_specific
-    # train set: the training set includes 80 participants data
-    # test set: the test set for cross-dataset and within-dataset evaluations
-    # test_person_specific: evaluation subset for the person specific setting
-    sub_folder_use = 'train'
-    train_set = GazeDataset(dataset_path=data_dir, keys_to_use=datastore[sub_folder_use], sub_folder=sub_folder_use,
-                            transform=trans, is_shuffle=is_shuffle, is_load_label=True)
-    train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=num_workers)
+        self.train = train  # training set or test set
+        self.subjects = 'train' if self.train else 'test'
 
-    return train_loader
+        self.data = []
 
+        with open(os.path.join(root, 'train_test_split.json'), 'r') as f:
+            data_list = json.load(f)
 
-def get_test_loader(data_dir, batch_size, num_workers=4, is_shuffle=True):
-    # load dataset
-    refer_list_file = os.path.join(data_dir, 'train_test_split.json')
-    print('load the test file list from: ', refer_list_file)
-
-    with open(refer_list_file, 'r') as f:
-        datastore = json.load(f)
-
-    # there are three subsets for ETH-XGaze dataset: train, test and test_person_specific
-    # train set: the training set includes 80 participants data
-    # test set: the test set for cross-dataset and within-dataset evaluations
-    # test_person_specific: evaluation subset for the person specific setting
-    sub_folder_use = 'test'
-    test_set = GazeDataset(dataset_path=data_dir, keys_to_use=datastore[sub_folder_use], sub_folder=sub_folder_use,
-                           transform=trans, is_shuffle=is_shuffle, is_load_label=False)
-    test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=num_workers)
-
-    return test_loader
-
-
-class GazeDataset(Dataset):
-    def __init__(self, dataset_path: str, keys_to_use: List[str] = None, sub_folder='', transform=None, is_shuffle=True,
-                 index_file=None, is_load_label=True):
-        self.path = dataset_path
-        self.hdfs = {}
-        self.sub_folder = sub_folder
-        self.is_load_label = is_load_label
-
-        # assert len(set(keys_to_use) - set(all_keys)) == 0
-        # Select keys
-        # TODO: select only people with sufficient entries?
-        self.selected_keys = [k for k in keys_to_use]
+        self.selected_keys = [k for k in data_list[self.subjects]]
         assert len(self.selected_keys) > 0
 
-        for num_i in range(0, len(self.selected_keys)):
-            file_path = os.path.join(self.path, self.sub_folder, self.selected_keys[num_i])
-            self.hdfs[num_i] = h5py.File(file_path, 'r', swmr=True)
-            # print('read file: ', os.path.join(self.path, self.selected_keys[num_i]))
-            assert self.hdfs[num_i].swmr_mode
+        for idx in range(len(self.selected_keys)):
+            hdf = h5py.File(os.path.join(self.root, self.subjects, self.selected_keys[idx]), 'r', swmr=True)
+            assert hdf.swmr_mode
+            self.data += [(idx, i) for i in range(hdf['face_patch'].shape[0])]
+            hdf.close()
 
-        # Construct mapping from full-data index to key and person-specific index
-        if index_file is None:
-            self.idx_to_kv = []
-            for num_i in range(0, len(self.selected_keys)):
-                n = self.hdfs[num_i]["face_patch"].shape[0]
-                self.idx_to_kv += [(num_i, i) for i in range(n)]
-        else:
-            print('load the file: ', index_file)
-            self.idx_to_kv = np.loadtxt(index_file, dtype=np.int)
+    def __getitem__(self, index):
+        key, datapoint = self.data[index]
+        hdf = h5py.File(os.path.join(self.root, self.subjects, self.selected_keys[key]), 'r', swmr=True)
+        assert hdf.swmr_mode
 
-        for num_i in range(0, len(self.hdfs)):
-            if self.hdfs[num_i]:
-                self.hdfs[num_i].close()
-                self.hdfs[num_i] = None
+        data = hdf['face_patch'][datapoint, :]
+        data = data[:, :, [2, 1, 0]]
+        target = hdf['face_gaze'][datapoint, :]
+        target = target.astype('float')
 
-        if is_shuffle:
-            random.shuffle(self.idx_to_kv)  # random the order to stable the training
+        if self.transform is not None:
+            data = self.transform(data)
 
-        self.hdf = None
-        self.transform = transform
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return data, target
 
     def __len__(self):
-        return len(self.idx_to_kv)
-
-    def __del__(self):
-        for num_i in range(0, len(self.hdfs)):
-            if self.hdfs[num_i]:
-                self.hdfs[num_i].close()
-                self.hdfs[num_i] = None
-
-    def __getitem__(self, idx):
-        key, idx = self.idx_to_kv[idx]
-
-        self.hdf = h5py.File(os.path.join(self.path, self.sub_folder, self.selected_keys[key]), 'r', swmr=True)
-        assert self.hdf.swmr_mode
-
-        # Get face image
-        image = self.hdf['face_patch'][idx, :]
-        image = image[:, :, [2, 1, 0]]  # from BGR to RGB
-        image = self.transform(image)
-
-        # Get labels
-        if self.is_load_label:
-            gaze_label = self.hdf['face_gaze'][idx, :]
-            gaze_label = gaze_label.astype('float')
-            return image, gaze_label
-        else:
-            return image
-
-
-if __name__ == '__main__':
-    trainloader = get_train_loader('/mnt/saves/ETH-XGaze/xgaze_224', batch_size=64, num_workers=0)
-    breakpoint()
+        return len(self.data)
