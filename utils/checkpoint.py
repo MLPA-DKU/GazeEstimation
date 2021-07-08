dedb49df5f8c4a62fb0e9488ab257200fd78f76d
winterchild <demie.oh@gmail.com>
2021-07-08 18:25:40 +0900

    Initial commit

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -5,8 +5,14 @@ import os
 import torch
 
 
-def create_checkpoint_handler(checkpoint_obj, f):
-    return CheckpointHandler(checkpoint_obj, f)
+def create_checkpoint_handler(
+        checkpoint_obj,
+        f
+    ):
+    return CheckpointHandler(
+        checkpoint_obj,
+        f
+        )
 
 
 class CheckpointHandler:
@@ -15,9 +21,13 @@ class CheckpointHandler:
             self,
             obj: Dict[str, int, torch.nn.Module, torch.optim.Optimizer],
             f: Union[str, os.PathLike, BinaryIO, IO[bytes]],
+            save_as_state_dict: bool = True,
+            save_best_model_only: bool = True,
         ):
         self.obj = obj
         self.f = f
+        self.save_state_dict = save_as_state_dict
+        self.save_best_model = save_best_model_only
 
     def save(self) -> None:
         try:
@@ -27,10 +37,19 @@ class CheckpointHandler:
         except Exception as e:
             logging.error(f'error occurs when saving checkpoint at {self.f} by "{e}"')
 
-    def load(self):
+    def load(self) -> Dict[str, int, torch.nn.Module, torch.optim.Optimizer]:
         try:
             logging.debug(f'trying to load checkpoint from {self.f}...')
-            ...
+            checkpoint = torch.load(self.f, map_location='')
             logging.debug(f'loading checkpoint from {self.f} successfully')
+            return checkpoint
         except Exception as e:
             logging.error(f'error occurs when loading checkpoint from {self.f} by "{e}"')
+
+
+#
+# volume/
+#   checkpoint_best_perf.pth
+#   tfevent.out.any....
+#   config.yaml
+#
